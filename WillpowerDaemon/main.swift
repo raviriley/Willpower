f67dc2d2c9625030c7c73c143ab09e36b4689744
Ravi Riley <raviriley@gmail.com>
2026-03-10 16:16:11 -0400

    Bump version to 1.0.10

--- WillpowerDaemon/main.swift
+++ WillpowerDaemon/main.swift
@@ -3,7 +3,7 @@ import WillpowerKit
 
 // MARK: - Configuration
 
-private let daemonVersion = "1.0.9"
+private let daemonVersion = "1.0.10"
 private let runLoopInterval: TimeInterval = 5.0
 private let heartbeatInterval: TimeInterval = 5.0
 
